ebd28e7567dc75856abe1e61a08bf4634106a26c
Rory Douglas <rory1douglas@gmail.com>
2018-02-22 15:14:21 -0500

    AnimatedSprite SetAnimation/Play test

--- src/dodge_player.c
+++ src/dodge_player.c
@@ -14,10 +14,9 @@ godot_variant ready(godot_object *p_instance, void *p_method_data, void *p_user_
 godot_variant process(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args);
 
 // call-out methods
+void play(godot_object *animated_sprite);
 void set_animation(godot_object *animated_sprite, char *anim_name);
-godot_node_path get_node_path(godot_object *instance);
 godot_object *get_node(godot_object *instance, char *node_name);
-godot_string get_node_name(godot_object *instance);
 
 void register_player(void *p_handle ) {
 	// register Player class
@@ -50,8 +49,6 @@ GDCALLINGCONV void *player_constructor(godot_object *p_instance, void *p_method_
 
 GDCALLINGCONV void player_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data) {
 	printf("Player freed\n");
-	// commenting out, no user data right now
-	//api->godot_free(p_user_data);
 }
 
 godot_variant ready(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
@@ -59,94 +56,20 @@ godot_variant ready(godot_object *p_instance, void *p_method_data, void *p_user_
 
 	printf( "ready!\n" );
 
-	{
-		godot_node_path player_path = get_node_path(p_instance);
-		godot_string player_path_gstring = api->godot_node_path_as_string(&player_path);
-		godot_char_string player_path_cstring = api->godot_string_utf8(&player_path_gstring);
-		printf("Player path= %s\n", api->godot_char_string_get_data(&player_path_cstring));
-
-		api->godot_string_destroy(&player_path_gstring);
-		api->godot_char_string_destroy(&player_path_cstring);
-		api->godot_node_path_destroy(&player_path);
-	}
-
-	{
-		godot_string node_name = get_node_name(p_instance);
-		godot_char_string node_name_cstring = api->godot_string_utf8(&node_name);
-		printf("Node name== %s\n", api->godot_char_string_get_data(&node_name_cstring));
-
-		api->godot_string_destroy(&node_name);
-		api->godot_char_string_destroy(&node_name_cstring);
-	}
-
 	godot_object* as = get_node(p_instance, "AnimatedSprite");
-
 	set_animation(as, "up");
-
-	api->godot_object_destroy(as);
 
 	return ret;
 }
 
 godot_variant process(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
 	godot_variant ret;
-	//printf( "process\n" );
+	printf( "process\n" );
 	
-	{
-		godot_node_path player_path = get_node_path(p_instance);
-		godot_string player_path_gstring = api->godot_node_path_as_string(&player_path);
-		godot_char_string player_path_cstring = api->godot_string_utf8(&player_path_gstring);
-		//printf("Player path= %s\n", api->godot_char_string_get_data(&player_path_cstring));
-
-		api->godot_string_destroy(&player_path_gstring);
-		api->godot_char_string_destroy(&player_path_cstring);
-		api->godot_node_path_destroy(&player_path);
-	}
-
-	{
-		godot_string node_name = get_node_name(p_instance);
-
-		godot_char_string node_name_cstring = api->godot_string_utf8(&node_name);
-		//printf("Node name== %s\n", api->godot_char_string_get_data(&node_name_cstring));
-
-		api->godot_string_destroy(&node_name);
-		api->godot_char_string_destroy(&node_name_cstring);
-	}
+	godot_object* as = get_node(p_instance, "AnimatedSprite");
+	play(as);
 
 	return ret;
-}
-
-godot_string get_node_name(godot_object *instance) {
-	char *class_name = "Node";
-	char *method_name = "get_name";
-
-	static godot_method_bind *mb = NULL;
-	if (!mb) {
-		mb = api->godot_method_bind_get_method(class_name, method_name);
-	}
-
-	godot_string nm;
-	const void *c_args[1] = {};
-
-	api->godot_method_bind_ptrcall(mb, instance, c_args, &nm);
-
-	return nm;
-}
-
-godot_node_path get_node_path(godot_object *instance) {
-	char *class_name = "Node";
-	char *method_name = "get_path";
-
-	static godot_method_bind *mb = NULL;
-	if (!mb) {
-		mb = api->godot_method_bind_get_method(class_name, method_name);
-	}
-
-	godot_node_path np;
-	const void *c_args[1] = {};
-
-	api->godot_method_bind_ptrcall(mb, instance, c_args, &np);
-	return np;
 }
 
 godot_object *get_node(godot_object *instance, char *node_name) {
@@ -189,6 +112,26 @@ void set_animation(godot_object *animated_sprite, char *anim_name) {
 	godot_string name;
 	api->godot_string_new(&name);
 	api->godot_string_parse_utf8(&name, anim_name);
+	const void *c_args[] = {
+		&name
+	};
+
+	api->godot_method_bind_ptrcall(mb, animated_sprite, c_args, NULL);
+	api->godot_string_destroy(&name);
+}
+
+void play(godot_object *animated_sprite) {
+	char *class_name = "AnimatedSprite";
+	char *method_name = "play";
+
+	static godot_method_bind *mb = NULL;
+	if (!mb) {
+		mb = api->godot_method_bind_get_method(class_name, method_name);
+	}
+
+	godot_string name;
+	api->godot_string_new(&name);
+	api->godot_string_parse_utf8(&name, "");
 	const void *c_args[] = {
 		&name
 	};
